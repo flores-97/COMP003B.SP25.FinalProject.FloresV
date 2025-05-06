cc106486c76fc41ac130a99d53153dbe635c717b
flores-97 <floresvictor15@gmail.com>
2025-05-06 16:49:05 -0700

    installed swashbuckle package

--- COMP003B.SP25.FinalProject.FloresV/Program.cs
+++ COMP003B.SP25.FinalProject.FloresV/Program.cs
@@ -24,9 +24,8 @@ namespace COMP003B.SP25.FinalProject.FloresV
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
