bc36a6b0e140c162edcd3fcf86e866f8c8cc56bf
flores-97 <floresvictor15@gmail.com>
2025-05-06 09:05:57 -0700

    created veiws and controllers

---
+++ COMP003B.SP25.FinalProject.FloresV/Controllers/RanksController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using COMP003B.SP25.FinalProject.FloresV.Data;
+using COMP003B.SP25.FinalProject.FloresV.Models;
+
+namespace COMP003B.SP25.FinalProject.FloresV.Controllers
+{
+    public class RanksController : Controller
+    {
+        private readonly WebDevAcademyContext _context;
+
+        public RanksController(WebDevAcademyContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Ranks
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Ranks.ToListAsync());
+        }
+
+        // GET: Ranks/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rank = await _context.Ranks
+                .FirstOrDefaultAsync(m => m.RankId == id);
+            if (rank == null)
+            {
+                return NotFound();
+            }
+
+            return View(rank);
+        }
+
+        // GET: Ranks/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Ranks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("RankId,Title")] Rank rank)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(rank);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(rank);
+        }
+
+        // GET: Ranks/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rank = await _context.Ranks.FindAsync(id);
+            if (rank == null)
+            {
+                return NotFound();
+            }
+            return View(rank);
+        }
+
+        // POST: Ranks/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("RankId,Title")] Rank rank)
+        {
+            if (id != rank.RankId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(rank);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RankExists(rank.RankId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(rank);
+        }
+
+        // GET: Ranks/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rank = await _context.Ranks
+                .FirstOrDefaultAsync(m => m.RankId == id);
+            if (rank == null)
+            {
+                return NotFound();
+            }
+
+            return View(rank);
+        }
+
+        // POST: Ranks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var rank = await _context.Ranks.FindAsync(id);
+            if (rank != null)
+            {
+                _context.Ranks.Remove(rank);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RankExists(int id)
+        {
+            return _context.Ranks.Any(e => e.RankId == id);
+        }
+    }
+}
---
+++ COMP003B.SP25.FinalProject.FloresV/Migrations/20250506155404_InitialCreate.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace COMP003B.SP25.FinalProject.FloresV.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Members",
+                columns: table => new
+                {
+                    MemberId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Members", x => x.MemberId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ranks",
+                columns: table => new
+                {
+                    RankId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ranks", x => x.RankId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Branches",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MemberId = table.Column<int>(type: "int", nullable: false),
+                    RankId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Branches", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Branches_Members_MemberId",
+                        column: x => x.MemberId,
+                        principalTable: "Members",
+                        principalColumn: "MemberId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Branches_Ranks_RankId",
+                        column: x => x.RankId,
+                        principalTable: "Ranks",
+                        principalColumn: "RankId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Branches_MemberId",
+                table: "Branches",
+                column: "MemberId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Branches_RankId",
+                table: "Branches",
+                column: "RankId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Branches");
+
+            migrationBuilder.DropTable(
+                name: "Members");
+
+            migrationBuilder.DropTable(
+                name: "Ranks");
+        }
+    }
+}
