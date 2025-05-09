3aff3cdc34171dbc4134edb86aba56d40ab67818
flores-97 <floresvictor15@gmail.com>
2025-05-09 07:43:57 -0700

    reconfig program.cs in order to retrieve swagger. Swagger now open but does not have items

--- COMP003B.SP25.FinalProject.FloresV/Program.cs
+++ COMP003B.SP25.FinalProject.FloresV/Program.cs
@@ -17,17 +17,20 @@ namespace COMP003B.SP25.FinalProject.FloresV
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
             builder.Services.AddDbContext<WebDevAcademyContext>(options => options.UseSqlServer("Name=ConnectionStrings:DefaultConnection"));
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
